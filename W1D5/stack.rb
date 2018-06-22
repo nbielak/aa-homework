904aa0895d58df940b6c377cde2694eb4ce54004
your name here <nickbielak95@gmail.com>
2018-06-21 21:21:31 -0700

    Create stack.rb

---
+++ W1D5/stack.rb
@@ -0,0 +1,30 @@
+class Stack
+  def initialize(arr=Array.new)
+    # create ivar to store stack here!
+    @stack = arr
+  end
+
+  def [](el)
+    @stack[el]
+  end
+
+  def push(el)
+    # adds an element to the stack
+    @stack << el
+  end
+
+  def pop
+    # removes one element from the stack
+    @stack = @stack[0..-2]
+    @stack[-1]
+  end
+
+  def peek
+    # returns, but doesn't remove, the top element in the stack
+    @stack[-1]
+  end
+end
+
+p stack = Stack.new([1, 2, 3, 4, 5])
+p stack.push(6)
+p stack.pop
